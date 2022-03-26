13d822c8d4c462e09654115873068411e5f47613
Mateusz Dudzinski <dudzinskimat@outlook.com>
2022-03-26 12:50:42 +0100

    implement working block travel

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,56 +1,121 @@
 import * as vscode from 'vscode';
 
-function emptyLineBelow(editor: vscode.TextEditor, hungry: boolean) {
+function getLineIndexUp(editor: vscode.TextEditor, hungry: boolean): vscode.Position {
     const document = editor.document;
-    let line = editor.selection.active.line;
-    let max = document.lineCount - 1;
+    let lineIdx = editor.selection.active.line;
+    
+    // If we are on the whitespace line, but text island starts in the prev line and we 
+    // are in hungry more, then we want to skip the island only, so it's easiest to just 
+    // start from the prev line
+    if (!hungry && 
+        lineIdx > 0 && 
+        document.lineAt(lineIdx).isEmptyOrWhitespace && 
+        !document.lineAt(lineIdx - 1).isEmptyOrWhitespace) {
+        lineIdx--;
+    }
 
-    // First, if we are in whitespace we first need to skip to the end of an island
-    if (document.lineAt(line).isEmptyOrWhitespace) {
-        while (line < max && document.lineAt(line + 1).isEmptyOrWhitespace) {
-            line++;
-        }
-
-        if (line === max) { // TODO: Perhaps not necesarry?
-            return document.lineAt(line);
+    if (document.lineAt(lineIdx).isEmptyOrWhitespace) {
+        while (lineIdx > 0 && document.lineAt(lineIdx - 1).isEmptyOrWhitespace) {
+            lineIdx--;
         }
         
-        // TODO: Don't do this if not in hungry mode?
-        line++;
+        // If not in hungry mode, or we're already at the start, stop here
+        if (!hungry || lineIdx === 0) {
+            return new vscode.Position(lineIdx, 0);
+        }
+
+        lineIdx--;
     }
 
-    while (line < max && !document.lineAt(line).isEmptyOrWhitespace) {
-        line++;
+    while (lineIdx > 0 && !document.lineAt(lineIdx).isEmptyOrWhitespace) {
+        lineIdx--;
     }
 
-    return document.lineAt(line);
+    return new vscode.Position(lineIdx, 0);
 }
 
-function travelParagraphDown(editor: vscode.TextEditor, hungry: boolean, select: boolean) {
-    const line = emptyLineBelow(editor, hungry);
-    const newPosn = new vscode.Position(line.lineNumber, line.text.length); // End of line, in case is last line
-    editor.selection = new vscode.Selection(select ? editor.selection.anchor : newPosn, newPosn);
-    editor.revealRange(new vscode.Range(newPosn, newPosn));
+function getLineIndexDown(editor: vscode.TextEditor, hungry: boolean): vscode.Position {
+    const document = editor.document;
+    const lastLineIdx = document.lineCount - 1;
+    let lineIdx = editor.selection.active.line;
+    
+    // If we are on the whitespace line, but text island starts in the next line and we 
+    // are in hungry more, then we want to skip the island only, so it's easiest to just 
+    // start from the next line
+    if (!hungry && 
+        lineIdx < lastLineIdx && 
+        document.lineAt(lineIdx).isEmptyOrWhitespace && 
+        !document.lineAt(lineIdx + 1).isEmptyOrWhitespace) {
+        lineIdx++;
+    }
+
+    if (document.lineAt(lineIdx).isEmptyOrWhitespace) {
+        while (lineIdx < lastLineIdx && document.lineAt(lineIdx + 1).isEmptyOrWhitespace) {
+            lineIdx++;
+        }
+        
+        // If not in hungry mode, or we're already at the end, stop here
+        if (!hungry || lineIdx === lastLineIdx) {
+            const idxInLine = lineIdx === lastLineIdx ? document.lineAt(lineIdx).text.length : 0;
+            return new vscode.Position(lineIdx, idxInLine);
+        }
+
+        lineIdx++;
+    }
+
+    while (lineIdx < lastLineIdx && !document.lineAt(lineIdx).isEmptyOrWhitespace) {
+        lineIdx++;
+    }
+
+    const idxInLine = lineIdx === lastLineIdx ? document.lineAt(lineIdx).text.length : 0;
+    return new vscode.Position(lineIdx, idxInLine);
+}
+
+function travelPara(editor: vscode.TextEditor, moveDown: boolean, hungry: boolean, select: boolean) {
+    const moveTo = moveDown ? getLineIndexDown(editor, hungry) : getLineIndexUp(editor, hungry);
+    editor.selection = new vscode.Selection(select ? editor.selection.anchor : moveTo, moveTo);
+    editor.revealRange(new vscode.Range(moveTo, moveTo));
 }
 
 export function activate(context: vscode.ExtensionContext)  {
-	console.log('Congratulations, your extension "test1" is now active in the web extension host!');
+	// TODO: log something?
 
-	let disposable = vscode.commands.registerCommand('test1.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from test1 in a web extension host!');
-	});
-
-    var jumpDown = vscode.commands.registerTextEditorCommand("test1.travelParagraphDown", function(editor) {
-        travelParagraphDown(editor, true, false);
+    var jumpDown = vscode.commands.registerTextEditorCommand("test1.paraDown", function(editor) {
+        travelPara(editor, true, false, false);
     });
 
-    var jumpDownSelect = vscode.commands.registerTextEditorCommand("test1.selectParagraphDown", function(editor) {
-        travelParagraphDown(editor, true, true);
+    var jumpDownSelect = vscode.commands.registerTextEditorCommand("test1.paraDownSelect", function(editor) {
+        travelPara(editor, true, false, true);
     });
 
-    context.subscriptions.push(disposable, jumpDown, jumpDownSelect);
+    var jumpDownHungry = vscode.commands.registerTextEditorCommand("test1.paraDownHungry", function(editor) {
+        travelPara(editor, true, true, false);
+    });
+
+    var jumpDownSelectHungry = vscode.commands.registerTextEditorCommand("test1.paraDownSelectHungry", function(editor) {
+        travelPara(editor, true, true, true);
+    });
+
+    var jumpUp = vscode.commands.registerTextEditorCommand("test1.paraUp", function(editor) {
+        travelPara(editor, false, false, false);
+    });
+
+    var jumpUpSelect = vscode.commands.registerTextEditorCommand("test1.paraUpSelect", function(editor) {
+        travelPara(editor, false, false, true);
+    });
+
+    var jumpUpHungry = vscode.commands.registerTextEditorCommand("test1.paraUpHungry", function(editor) {
+        travelPara(editor, false, true, false);
+    });
+
+    var jumpUpSelectHungry = vscode.commands.registerTextEditorCommand("test1.paraUpSelectHungry", function(editor) {
+        travelPara(editor, false, true, true);
+    });
+
+    context.subscriptions.push(
+        jumpDown, jumpDownSelect, jumpDownHungry, jumpDownSelectHungry,
+        jumpUp, jumpUpSelect, jumpUpHungry, jumpUpSelectHungry);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
